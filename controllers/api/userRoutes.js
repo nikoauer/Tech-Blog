8e47decd030ce274a1782248d610b389ce2348b3
Niko Auer <nikoauer26@gmail.com>
2023-08-25 15:32:26 +1000

    added login function api

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,9 +1,9 @@
 //localhost:3001/api/users
 const router = require("express").Router();
 const { User } = require("../../models");
+
 router.post("/", async (req, res) => {
     try {
-        
         console.log("Api endpoint piged to add user");
         const newUser = await User.create(req.body);
         req.session.save(() => {
@@ -16,5 +16,37 @@ router.post("/", async (req, res) => {
         console.log(error)
     }
 });
+
+router.post('/login', async (req, res) => {
+    try {
+      const userData = await User.findOne({ where: { email: req.body.email } });
+  
+      if (!userData) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect email or password, please try again' });
+        return;
+      }
+  
+      const validPassword = await userData.checkPassword(req.body.password);
+  
+      if (!validPassword) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect email or password, please try again' });
+        return;
+      }
+  
+      req.session.save(() => {
+        req.session.user_id = userData.id;
+        req.session.logged_in = true;
+        
+        res.json({ user: userData, message: 'You are now logged in!' });
+      });
+  
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
 
 module.exports = router;
