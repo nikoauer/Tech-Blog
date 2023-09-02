511fe932f41ed659a2592c1f111d91cbf735a790
Niko Auer <nikoauer26@gmail.com>
2023-09-02 12:37:15 +1000

    changed html and css for login/main

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -10,8 +10,6 @@ router.get('/login', (req, res) => {
 })
 
 
-router.get('/signup', (req, res) => {
-    res.render('signup')
-})
+
 
 module.exports = router
--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create();
 
 const sess = {
-  secret: 'Super secret secret',
+  secret: 'Secret Key',
   cookie: {
     maxAge: 300000,
     httpOnly: true,
@@ -41,5 +41,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
 });
